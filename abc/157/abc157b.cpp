884eabd8c93fd4960cddfc1c585ed1c1d3683a17
bakaiton <itubokaito@yahoo.co.jp>
2020-03-31 20:14:39 +0900

    B if文の条件を変更 AC

--- abc/157/abc157b.cpp
+++ abc/157/abc157b.cpp
@@ -12,7 +12,7 @@ int main(){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
             cin>>a[i][j];
-            //cout<<a[i][j]<<endl;
+            aflag[i][j] = false;
         }
     }
     cin>>n;
@@ -30,19 +30,19 @@ int main(){
 
     for (int i = 0; i < 3; i++){
         //縦横
-        if (aflag[i][0] == aflag[i][1] && aflag[i][1] == aflag[i][2]){
+        if (aflag[i][0] && aflag[i][1] && aflag[i][2]){
             flag = 1;
         }
-        if (aflag[0][i] == aflag[1][i] && aflag[1][i] == aflag[2][i]){
+        if (aflag[0][i] && aflag[1][i] && aflag[2][i]){
             flag = 1;
         }
-        //斜め
-        if (aflag[0][0] == aflag[1][1] && aflag[1][1] == aflag[2][2]){
-            flag = 1;
-        }
-        if (aflag[0][2] == aflag[1][1] && aflag[1][1] == aflag[2][0]){
-            flag = 1;
-        }
+    }
+    //斜め
+    if (aflag[0][0] && aflag[1][1] && aflag[2][2]){
+        flag = 1;
+    }
+    if (aflag[0][2] && aflag[1][1] && aflag[2][0]){
+        flag = 1;
     }
     cout<<ans[flag]<<endl;
 }
