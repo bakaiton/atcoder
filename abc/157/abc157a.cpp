3c3ab3c99beaa16f3a9c667fd2d7cc077cc29046
bakaiton <itubokaito@yahoo.co.jp>
2020-03-31 19:29:16 +0900

    A 提出AC

---
+++ abc/157/abc157a.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
+    float n;
+    int ans = 0;
+    cin>>n;
+    ans = n/2.0+0.5;
+    cout<<ans<<endl;
+}
